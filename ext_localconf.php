a78caa8f73ab0b4844ecb3e87120464f201d3ddf
Patrick Finkbeiner <finkbeiner.patrick@gmail.com>
2022-08-24 12:48:54 +0200

    Fix & format localconf and tables.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,43 +1,46 @@
 <?php
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-$_EXTKEY = 'aoe_ipauth';
-$backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('backend');
+defined('TYPO3') or die();
 
-if ('BE' === TYPO3_MODE) {
-    // Do not show the IP records in the listing
-    $allowedTablesTs = '
-		mod.web_list.deniedNewTables := addToList(tx_aoeipauth_domain_model_ip)
-		mod.web_list.hideTables := addToList(tx_aoeipauth_domain_model_ip)
-	';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($allowedTablesTs);
+(static function() {
+    $_EXTKEY = 'aoe_ipauth';
 
-    // Hooks
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = \AOE\AoeIpauth\Hooks\Tcemain::class;
-} elseif ('FE' === TYPO3_MODE) {
-    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-    )->get($_EXTKEY);
-    $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] =
-        isset($extensionConfiguration['fetchFeUserIfNoSession']) ? boolval($extensionConfiguration['fetchFeUserIfNoSession']) : 1;
-    unset($extensionConfiguration);
-}
+    $backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('backend');
 
-// IP Authentication Service
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'tx_aoeipauth_typo3_service_authentication',
-    array(
-        'title' => 'IP Authentication',
-        'description' => 'Authenticates against IP addresses and ranges.',
-        'subtype' => 'authUserFE,getUserFE,getGroupsFE',
-        'available' => true,
-        // Must be higher than for tx_sv_auth (50) or tx_sv_auth will deny request unconditionally
-        'priority' => 80,
-        'quality' => 50,
-        'os' => '',
-        'exec' => '',
-        'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Typo3/Service/Authentication.php',
-        'className' => 'AOE\AoeIpauth\Typo3\Service\Authentication',
-    )
-);
+    if ('BE' === TYPO3_MODE) {
+        // Do not show the IP records in the listing
+        $allowedTablesTs = '
+        mod.web_list.deniedNewTables := addToList(tx_aoeipauth_domain_model_ip)
+        mod.web_list.hideTables := addToList(tx_aoeipauth_domain_model_ip)
+    ';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($allowedTablesTs);
+
+        // Hooks
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = \AOE\AoeIpauth\Hooks\Tcemain::class;
+    } elseif ('FE' === TYPO3_MODE) {
+        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+        )->get($_EXTKEY);
+        $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] =
+            isset($extensionConfiguration['fetchFeUserIfNoSession']) ? boolval($extensionConfiguration['fetchFeUserIfNoSession']) : 1;
+        unset($extensionConfiguration);
+    }
+
+    // IP Authentication Service
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'tx_aoeipauth_typo3_service_authentication',
+        array(
+            'title' => 'IP Authentication',
+            'description' => 'Authenticates against IP addresses and ranges.',
+            'subtype' => 'authUserFE,getUserFE,getGroupsFE',
+            'available' => true,
+            // Must be higher than for tx_sv_auth (50) or tx_sv_auth will deny request unconditionally
+            'priority' => 80,
+            'quality' => 50,
+            'os' => '',
+            'exec' => '',
+            'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Typo3/Service/Authentication.php',
+            'className' => 'AOE\AoeIpauth\Typo3\Service\Authentication',
+        )
+    );
+})();
--- ext_tables.php
+++ ext_tables.php
@@ -1,29 +1,29 @@
 <?php
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
-// IP
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_aoeipauth_domain_model_ip',
-    'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tx_aoeipauth_domain_model_ip.xlf'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'fe_users',
-    'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_fe_users.xlf'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'fe_groups',
-    'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_fe_groups.xlf'
-);
-
-if ('BE' === TYPO3_MODE) {
-    // registering reports
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$_EXTKEY] = array(
-        'AOE\\AoeIpauth\\Report\\IpGroupAuthenticationStatus',
-        'AOE\\AoeIpauth\\Report\\IpUserAuthenticationStatus',
+(function() {
+    ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_aoeipauth_domain_model_ip',
+        'EXT:aoe_ipauth/Resources/Private/Language/locallang_csh_tx_aoeipauth_domain_model_ip.xlf'
     );
-}
+
+    ExtensionManagementUtility::addLLrefForTCAdescr(
+        'fe_users',
+        'EXT:aoe_ipauth/Resources/Private/Language/locallang_csh_fe_users.xlf'
+    );
+
+    ExtensionManagementUtility::addLLrefForTCAdescr(
+        'fe_groups',
+        'EXT:aoe_ipauth/Resources/Private/Language/locallang_csh_fe_groups.xlf'
+    );
+
+    if ('BE' === TYPO3_MODE) {
+        // registering reports
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$_EXTKEY] = array(
+            \AOE\AoeIpauth\Report\IpGroupAuthenticationStatus::class,
+            \AOE\AoeIpauth\Report\IpUserAuthenticationStatus::class,
+        );
+    }
+})();
